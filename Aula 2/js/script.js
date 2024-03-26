76dac29dd68f8a9dbfcd93f4ecc8c6aa2f79086e
Mazzi <thiago.mazzi2017@gmail.com>
2024-03-26 10:16:39 -0300

    Alterações dia 23/03

--- Aula 2/js/script.js
+++ Aula 2/js/script.js
@@ -1,3 +1,12 @@
-alerta = () => { 
-    alert(`Olá eu sou um alerta`);
-}
+alerta = () => {
+  alert(`Olá eu sou um alerta`);
+};
+const enviar = () => {
+  let nome = document.getElementById("Nome").value;
+  document.getElementById("resposta").innerHTML = `<h2>Olá ${nome} formulário enviado com sucesso</h2>`
+  if(nome ==""){
+    alert(`Olá bem vindo`)
+  }
+  else{
+  alert(`Olá ${nome}, seja bem vindo ao meu sistema!`);
+}};
